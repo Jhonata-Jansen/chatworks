5eabcd4b3b9bed9f419695b49baf0439a13d4a4b
Vinicius Mesel <vinicius@bigsp.com.br>
2017-04-05 18:03:04 -0300

    Setup added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='chatworks',
+      version='0.0.1',
+      description='ChatWorks library for enabling chatbots to work with multiple apis',
+      author='Vinicius Mesel',
+      author_email='bioinfo.vinicius@gmail.com',
+      install_requires=['requests', 'python-decouple'],
+      packages=['chatworks'])
